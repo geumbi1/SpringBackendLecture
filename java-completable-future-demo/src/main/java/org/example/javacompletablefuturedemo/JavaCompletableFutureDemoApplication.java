a642d15fdb05da5f4295f5441719944dac00fa52
geumbi <gkrry0404@naver.com>
2026-05-27 01:20:36 +0900

    5월26일(화)

---
+++ java-completable-future-demo/src/main/java/org/example/javacompletablefuturedemo/JavaCompletableFutureDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.javacompletablefuturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCompletableFutureDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaCompletableFutureDemoApplication.class, args);
+  }
+
+}
---
+++ java-executor-future-demo/src/main/java/org/example/javaexecutorfuturedemo/JavaExecutorFutureDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaexecutorfuturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExecutorFutureDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaExecutorFutureDemoApplication.class, args);
+  }
+
+}
